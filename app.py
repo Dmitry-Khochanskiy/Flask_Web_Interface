9b9edb30c3f7caf60bc691f4e1843218e3dd38b6
Dmitry Khochanskiy <demetros87@gmail.com>
2022-09-21 16:01:00 +0300

    Basic structure

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+def run():
+    app.run(debug=True, port=5690, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True)
